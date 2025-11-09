2cf1cb72f1bdb13782013dd3784f99bf93538461
Akash P <akashswapnam2007@gmail.com>
2025-11-09 10:01:38 +0530

    Corrected prime number generation for Noldbach problem
    Replaced prime number generation method with the Sieve of Eratosthenes.

--- Number theory/Noldbach_problem.py
+++ Number theory/Noldbach_problem.py
@@ -1,9 +1,10 @@
-n = int(input("Enter the limit : "))
-prime = list(range(2,n+1))
-for p in prime:
-    for num in prime:
-        if num%p==0 and num!=p:
-            prime.remove(num)
+#CREATE A LIST OF PRIME NUMBERS (Corrected)
+isprime = [True]*(n+1)
+for p in range(2, int(n**0.5)+1):
+    if isprime[p]:
+        for x in range(p*p, n+1, p):
+            isprime[x] = False
+prime = [i for i in range(2, n+1) if isprime[i]]
 
 # Search Noldbach Numbers
 for i in range(len(prime)-1):
