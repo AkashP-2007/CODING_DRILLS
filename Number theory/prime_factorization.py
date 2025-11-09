502a21069d2f46c047dc3d2e196a6e21b4ccb793
Akash P <akashswapnam2007@gmail.com>
2025-11-09 10:05:57 +0530

    Refactor prime number listing using Sieve of Eratosthenes
    Updated the prime number listing algorithm to use the Sieve of Eratosthenes for better efficiency.

--- Number theory/prime_factorization.py
+++ Number theory/prime_factorization.py
@@ -1,10 +1,11 @@
-# PRIME NUMBER listing using sieve of eratosthenes
 n = int(input("Enter the limit: "))
-l = list(range(2,n+1))
-for p in l:
-    for num in l:
-        if(num%p == 0 and num != p):
-            l.remove(num)
+#CREATE A LIST OF PRIME NUMBERS (Corrected)
+isprime = [True]*(n+1)
+for p in range(2, int(n**0.5)+1):
+    if isprime[p]:
+        for x in range(p*p, n+1, p):
+            isprime[x] = False
+l = [i for i in range(2, n+1) if isprime[i]]
 print(l)
 
 # prime factorization
