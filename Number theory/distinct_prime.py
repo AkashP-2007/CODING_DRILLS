559b07a22f5c4fac55621e0febc7543a0f86dc88
Akash P <akashswapnam2007@gmail.com>
2025-11-09 10:03:51 +0530

    Correct prime number generation method
    Replaced the prime number generation logic with the Sieve of Eratosthenes algorithm for efficiency.

--- Number theory/distinct_prime.py
+++ Number theory/distinct_prime.py
@@ -3,11 +3,13 @@ l = list(range(2,n+1))
 count = [0 for _ in range(n+1)]
 lucky_number = []
 
-#CREATE A LIST OF PRIME NUMBERS
-for p in l:
-    for num in l:
-        if num%p == 0 and num!=p :
-            l.remove(num)
+#CREATE A LIST OF PRIME NUMBERS (Corrected)
+isprime = [True]*(n+1)
+for p in range(2, int(n**0.5)+1):
+    if isprime[p]:
+        for x in range(p*p, n+1, p):
+            isprime[x] = False
+l = [i for i in range(2, n+1) if isprime[i]]
 
 #COUNT PRIME FACTORS          
 for p in l:
